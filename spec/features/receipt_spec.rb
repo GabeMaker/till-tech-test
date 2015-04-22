58873b2487a7383b051c1accbbe457fbc4365ade
gabe <GabeMaker@users.noreply.github.com>
2015-04-22 15:19:05 +0100

    refactor tests

--- spec/features/receipt_spec.rb
+++ spec/features/receipt_spec.rb
@@ -2,25 +2,6 @@ require 'till'
 
 feature 'As a customer I want to see the total cost of what I order' do
 
-  scenario 'I order a Cafe Latte' do
-    till = Till.new
-    till.order "Cafe Latte"
-    expect(till.total).to eq 4.75
-  end
-
-  scenario 'I order a Cappucino' do
-    till = Till.new
-    till.order "Cappucino"
-    expect(till.total).to eq 3.85
-  end
-
-  scenario 'I order a Cappucino and a Cafe Latte' do
-    till = Till.new
-    till.order "Cappucino"
-    till.order "Cafe Latte"
-    expect(till.total).to eq 8.6
-  end
-
   scenario 'I order 2 Cafe Lattes, a Blueberry Muffin and a Chocolate Mudcake' do
     till = Till.new
     till.order "Cappucino", 2
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -1,7 +1,26 @@
-describe 'Till' do
+describe Till do
+
+  let (:till) {Till.new}
+
+  it 'can take a single-item order' do
+    till.order "Cafe Latte"
+    expect(till.total).to eq 4.75
+  end
+
+  it 'can take a different single-item order' do
+    till.order "Cappucino"
+    expect(till.total).to eq 3.85
+  end
+
+  it 'can take a multi-item order' do
+    till.order "Cappucino"
+    till.order "Cafe Latte"
+    expect(till.total).to eq 8.6
+  end
+
   it 'can take an optional quantity argument when ordering' do
-    till = Till.new
     expect(till).to respond_to(:order).with(1).argument
     expect(till).to respond_to(:order).with(2).arguments
   end
+
 end
